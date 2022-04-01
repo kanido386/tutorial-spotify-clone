a406d1d9259a51c749e023d3d8ebf43c02f2adf2
kanido386 <kanido386@gmail.com>
2022-04-01 14:57:45 +0800

    Plan before action

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,16 @@ import Image from 'next/image'
 const Home: NextPage = () => {
   return (
     <div className="">
-      <Head>
-        <title>Cool Spotify</title>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
-      </Head>
       <h1>Let's learn how to build a Spotify!</h1>
+
+      <main>
+        {/* Sidebar */}
+        {/* Center */}
+      </main>
+
+      <div>
+        {/* Player */}
+      </div>
     </div>
   )
 }
